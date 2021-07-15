20ab580f98118d10ad8647935cff29845f00181d
Maycon Rabelo <loganrabelo@gmail.com>
2021-07-15 11:37:34 -0300

    iniciando repositorio alurakut imersao react

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import MainGrid from '../src/components/MainGrid'
+import Box from '../src/components/Box'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+
+function ProfileSidebar(propriedades){
+  return(
+    <Box>
+      <img src={`https://github.com/${propriedades.githubUser}.png`} style={{ borderRadius: '8px' }} />  
+    </Box>
+  )
+}
+
+
+export default function Home() {
+
+  const githubUser = 'mayconrabelo';
+  const pessoasFavoritas = ['handersonbf','juunegreiros','omariosouto','peas','rafaballerini','marcobrunodev','TREZEabts']
+
+  return (
+    <>
+    <AlurakutMenu />
+    <MainGrid>
+      <div className="profileArea" style={{ gridArea: 'profileArea' }}>
+        <ProfileSidebar githubUser={githubUser}/>
+      </div>
+      <div className="welcomeArea" style={{ gridArea: 'welcomeArea' }}>
+        <Box >
+          <h1 className="title">
+          Bem vindo(a)
+          </h1>
+
+          <OrkutNostalgicIconSet />
+
+        </Box>
+      </div>
+      <div className="profileRelationsArea" style={{ gridArea: 'profileRelationsArea' }}>
+        <ProfileRelationsBoxWrapper>
+          <h2 className="smallTitle" >
+          Pessoas da comunidade ({pessoasFavoritas.length})
+          </h2>
+          <ul>
+            {pessoasFavoritas.map((itenAtual) => {
+              return(
+                <li>
+                  <a href={`/users/${itenAtual}`} key={itenAtual}>
+                    <img src={`https://github.com/${itenAtual}.png`} />
+                    <span>{itenAtual}</span>
+                  </a>
+                </li>
+              )
+            })}
+          </ul>
+        </ProfileRelationsBoxWrapper>
+      </div>
+    </MainGrid>
+    </>
+  )
+}
